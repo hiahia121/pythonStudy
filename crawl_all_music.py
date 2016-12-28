15b08cc1685c4a50b73bba9802a18a1cf66c97bf
hiahia121 <541056367@qq.com>
2016-12-28 11:46:00 +0800

    Update crawl_all_music.py

--- crawl_all_music.py
+++ crawl_all_music.py
@@ -8,50 +8,48 @@ import time
 
 full_url = []
 target_data = []
-url = "https://douban.fm/j/v2/songlist/explore?type=hot&genre=0&limit=20&sample_cnt=5"
-
-pages = urllib2.urlopen(url).read()
-get_json = json.loads(pages)
-
-
-for t in range(len(get_json)):
-    full_url.append('https://douban.fm/j/v2/songlist/{0}/?kbps=192'.format(get_json[t]["id"]))
+for w in range(2,18):
+    url = "https://douban.fm/j/v2/songlist/explore?type=hot&genre={0}&limit=20&sample_cnt=5".format(w)
+    pages = urllib2.urlopen(url).read()
+    get_json = json.loads(pages)
 
 
-for x in full_url:
-    print x
-    url_last_str = []
-    time.sleep(10)
-    html = urllib2.urlopen(x).read()
+    for t in range(len(get_json)):
+        full_url.append('https://douban.fm/j/v2/songlist/{0}/?kbps=192'.format(get_json[t]["id"]))
 
-    hjson = json.loads(html)
-    count = hjson['count']
 
-    for i in range(0, count):
-        sid = hjson["songs"][i]["sid"]
-        ssid = hjson["songs"][i]["ssid"]
-        last_str = sid + "g" + ssid
-        url_last_str.append(last_str)
+    for x in full_url:
+        print x
+        url_last_str = []
+        time.sleep(5)
+        html = urllib2.urlopen(x).read()
 
-    for j in range(len(url_last_str)):
-        url_last_str[j] = "https://douban.fm/j/v2/song/" + url_last_str[j]
+        hjson = json.loads(html)
+        count = hjson['count']
 
-    for q in url_last_str:
-        pages = urllib2.urlopen(q).read()
-        njson = json.loads(pages)
+        for i in range(0, count):
+            sid = hjson["songs"][i]["sid"]
+            ssid = hjson["songs"][i]["ssid"]
+            last_str = sid + "g" + ssid
+            url_last_str.append(last_str)
 
-        print pages
-        print "============================="
-        music_title = njson['title']
-        liked_count = str(njson['liked_count'])
-        target_data.append(music_title)
-        target_data.append('\t')
-        target_data.append(liked_count)
-        target_data.append('\n')
+        for j in range(len(url_last_str)):
+            url_last_str[j] = "https://douban.fm/j/v2/song/" + url_last_str[j]
 
-        file_music = open('testmusic.txt', 'w')
-        for m in range(len(target_data)):
-            file_music.write(target_data[m])
+        for q in url_last_str:
+            pages = urllib2.urlopen(q).read()
+            njson = json.loads(pages)
 
-    time.sleep(5)
+            print pages
+            print "============================="
+            music_title = njson['title']
+            liked_count = str(njson['liked_count'])
+            target_data.append(music_title)
+            target_data.append('\t')
+            target_data.append(liked_count)
+            target_data.append('\n')
+
+            file_music = open('crawl_all_music.txt', 'w')
+            for m in range(len(target_data)):
+                file_music.write(target_data[m])
 file_music.close()
