11b0f922f98545f9bc1b9964c017905b890ef62f
hiahia121 <541056367@qq.com>
2016-12-30 18:13:11 +0800

    Update crawl_douban_all_music.py

--- crawl_douban_all_music.py
+++ crawl_douban_all_music.py
@@ -7,18 +7,26 @@ import json
 import time
 import string
 
-f = open("all_music_saved.txt","w")
+
+
+f = open("all_music2_saved.txt","w")
 def crawl_all_model_music():                     #爬下所有风格下，所有模块下的音乐＋喜欢数
+    count_style_music = 0
     for up_up_up_i in range(2,18):
+        time.sleep(10)
         up_up_url = "https://douban.fm/j/v2/songlist/explore?type=hot&genre={0}&limit=20&sample_cnt=5".format(up_up_up_i)
         crawl_one_model_music(up_up_url)
+        count_style_music += 1
+        print "###################################### " +count_style_music+ " style music has been crawled ###########################################"
 
 def crawl_one_model_music(up_up_url):                                                #爬下一个风格下，所有模块下的音乐名字＋喜欢数
+
     up_up_get_json = json.loads(urllib2.urlopen(up_up_url).read())
     for up_up_i in range(len(up_up_get_json)):
         up_url = "https://douban.fm/j/v2/songlist/{0}/?kbps=192".format(up_up_get_json[up_up_i]['id'])
         crawl_one_class_music(up_url)
-    print "###################################### one style music has been crawled ###########################################"
+
+
 
 def crawl_one_class_music(up_url):                                        #爬取一个模块下的所有音乐名字＋喜欢数
     save_music_list = []
