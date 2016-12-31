bacb56e764bd6454ca85de9a8366dcd8cab93d3a
hiahia121 <541056367@qq.com>
2016-12-31 10:51:49 +0800

    Update crawl_hotel_info.py

--- crawl_hotel_info.py
+++ crawl_hotel_info.py
@@ -13,8 +13,11 @@ def parse_html(html):
     content = etree.HTML(html)
     get_data1 = content.xpath('//h1[@class="shop-name"]/text()')
     get_data2 = content.xpath('//p[@class="info shop-star"]//span[@class="item"]/text()')
-    get_data = get_data1 + get_data2
+    get_data = get_data1
+    get_data.append('\t')
+    get_data += get_data2
     get_data[0] = get_data[0].replace("\n","").replace(" ","")
+    get_data[2] = get_data[2].replace("(","").replace(")","")
     print get_data
     return get_data
 
